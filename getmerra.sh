b16839cbe43d9cb187f9cc5a938a6e02d8adb24f
Steven Buczkowski <steven.buczkowski@gmail.com>
2016-06-20 15:09:44 -0400

    Generalized getmerra.sh to grab files needed for an arbitrary date

--- getmerra.sh
+++ getmerra.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-for day in {01..31}; do
+# Grabs one day of MERRA files necessary to collect all ERA-like
+# variables for repackaging
+#
+# Usage: getmerra <YYYY> <MM> <DD>
+#
+# Caveat: Present version stores downloaded files in the current directory
 
-    echo "Grabbing surface slv file for day ${day}"
-    wget http://goldsmr4.sci.gsfc.nasa.gov/opendap/MERRA2/M2T1NXSLV.5.12.4/2014/07/MERRA2_400.tavg1_2d_slv_Nx.201407${day}.nc4
-    echo "Grabbing surface rad file day day ${day}"
-    wget http://goldsmr4.sci.gsfc.nasa.gov/opendap/MERRA2/M2T1NXRAD.5.12.4/2014/07/MERRA2_400.tavg1_2d_rad_Nx.201407${day}.nc4
-    echo "Grabbing surface flx file for day ${day}"
-    wget http://goldsmr4.sci.gsfc.nasa.gov/opendap/MERRA2/M2T1NXFLX.5.12.4/2014/07/MERRA2_400.tavg1_2d_flx_Nx.201407${day}.nc4
-    echo "Grabbing levels asm file for day ${day}"
-    wget http://goldsmr5.sci.gsfc.nasa.gov/opendap/MERRA2/M2T3NVASM.5.12.4/2014/07/MERRA2_400.tavg3_3d_asm_Nv.201407${day}.nc4
+year=$1
+month=$2
+day=$3
 
-done
+echo "Grabbing surface slv file for ${year}/${month}/${day}"
+wget http://goldsmr4.sci.gsfc.nasa.gov/opendap/MERRA2/M2T1NXSLV.5.12.4/${year}/${month}/MERRA2_400.tavg1_2d_slv_Nx.${year}${month}${day}.nc4
+echo "Grabbing surface rad file for ${year}/${month}/${day}"
+wget http://goldsmr4.sci.gsfc.nasa.gov/opendap/MERRA2/M2T1NXRAD.5.12.4/${year}/${month}/MERRA2_400.tavg1_2d_rad_Nx.${year}${month}${day}.nc4
+echo "Grabbing surface flx file for ${year}/${month}/${day}"
+wget http://goldsmr4.sci.gsfc.nasa.gov/opendap/MERRA2/M2T1NXFLX.5.12.4/${year}/${month}/MERRA2_400.tavg1_2d_flx_Nx.${year}${month}${day}.nc4
+echo "Grabbing levels asm file for ${year}/${month}/${day}"
+wget http://goldsmr5.sci.gsfc.nasa.gov/opendap/MERRA2/M2T3NVASM.5.12.4/${year}/${month}/MERRA2_400.tavg3_3d_asm_Nv.${year}${month}${day}.nc4
+
+
