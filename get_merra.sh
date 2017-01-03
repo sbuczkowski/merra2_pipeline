cd3eef9e4e8f75d102fda9b15226665d4331619f
Steven Buczkowski <steven.buczkowski@gmail.com>
2017-01-03 13:51:55 -0500

    Before http->https changes

--- get_merra.sh
+++ get_merra.sh
@@ -11,13 +11,18 @@ year=$1
 month=$2
 day=$3
 
-echo "Grabbing surface slv file for ${year}/${month}/${day}"
-wget http://goldsmr4.sci.gsfc.nasa.gov/opendap/MERRA2/M2T1NXSLV.5.12.4/${year}/${month}/MERRA2_400.tavg1_2d_slv_Nx.${year}${month}${day}.nc4
-echo "Grabbing surface rad file for ${year}/${month}/${day}"
-wget http://goldsmr4.sci.gsfc.nasa.gov/opendap/MERRA2/M2T1NXRAD.5.12.4/${year}/${month}/MERRA2_400.tavg1_2d_rad_Nx.${year}${month}${day}.nc4
-echo "Grabbing surface flx file for ${year}/${month}/${day}"
-wget http://goldsmr4.sci.gsfc.nasa.gov/opendap/MERRA2/M2T1NXFLX.5.12.4/${year}/${month}/MERRA2_400.tavg1_2d_flx_Nx.${year}${month}${day}.nc4
-echo "Grabbing levels asm file for ${year}/${month}/${day}"
-wget http://goldsmr5.sci.gsfc.nasa.gov/opendap/MERRA2/M2T3NVASM.5.12.4/${year}/${month}/MERRA2_400.tavg3_3d_asm_Nv.${year}${month}${day}.nc4
+# echo "Grabbing surface asm file for ${year}/${month}/${day}"
+# wget http://goldsmr4.sci.gsfc.nasa.gov/opendap/MERRA2/M2I1NXASM.5.12.4/${year}/${month}/MERRA2_400.inst1_2d_asm_Nx.${year}${month}${day}.nc4
+# echo "Grabbing surface rad file for ${year}/${month}/${day}"
+# wget http://goldsmr4.sci.gsfc.nasa.gov/opendap/MERRA2/M2T1NXRAD.5.12.4/${year}/${month}/MERRA2_400.tavg1_2d_rad_Nx.${year}${month}${day}.nc4
+# echo "Grabbing surface flx file for ${year}/${month}/${day}"
+# wget http://goldsmr4.sci.gsfc.nasa.gov/opendap/MERRA2/M2T1NXFLX.5.12.4/${year}/${month}/MERRA2_400.tavg1_2d_flx_Nx.${year}${month}${day}.nc4
+# echo "Grabbing levels asm file for ${year}/${month}/${day}"
+# wget http://goldsmr5.sci.gsfc.nasa.gov/opendap/MERRA2/M2I3NVASM.5.12.4/${year}/${month}/MERRA2_400.inst3_3d_asm_Nv.${year}${month}${day}.nc4
+#
+echo "Grabbing levels aer (SO2) file for ${year}/${month}/${day}"
+wget --no-check-certificate http://goldsmr5.sci.gsfc.nasa.gov/opendap/MERRA2/M2I3NVAER.5.12.4/${year}/${month}/MERRA2_400.inst3_3d_aer_Nv.${year}${month}${day}.nc4
+echo "Grabbing levels chm (CO) file for ${year}/${month}/${day}"
+wget --no-check-certificate http://goldsmr5.sci.gsfc.nasa.gov/opendap/MERRA2/M2I3NVCHM.5.12.4/${year}/${month}/MERRA2_400.inst3_3d_chm_Nv.${year}${month}${day}.nc4
 
 
--- repackage_merra.sh
+++ repackage_merra.sh
@@ -1,28 +1,42 @@
-#!/bin/bash
+ #!/bin/bash
 
-for day in {01..31}; do
-    echo "Processing day $day"
+year=$1
+month=$2
+day=$3
 
-    echo "  Processing levels data..."
-    ncks -v T,QV,QL,QI,O3,CLOUD MERRA2_400.tavg3_3d_asm_Nv.201407${day}.nc4 -o merra2_201407${day}_levels.nc4
-    echo "    Renaming levels variables to match ERA..."
-    ncrename -h -O -v T,t -v QV,q -v O3,o3 -v QL,clwc -v QI,ciwc -v CLOUD,cc merra2_201407${day}_levels.nc4
+# NOTE: part of the process here is to rename variables and dimensions
+# in the MERRA namespace to replicate those in the ERA
+# namespace. Should changes be made in this mapping, they also must be
+# reflected in the subsequent read statements in
+# grib_interpolate_merra.m within rtp_prod2
 
-    echo "  Processing surface data..."
-    echo "    Extracting variables..."
-    ncks -v CLDTOT MERRA2_400.tavg1_2d_rad_Nx.201407${day}.nc4 -o cldtot.nc4
-    ncks -v FRSEAICE MERRA2_400.tavg1_2d_flx_Nx.201407${day}.nc4 -o frseaice.nc4
-    ncks -v PS,U10M,V10M,TS MERRA2_400.tavg1_2d_slv_Nx.201407${day}.nc4 -o merra2_201407${day}_surface.nc4
+echo "Processing day $day"
 
-    echo "    Appending variables..."
-    ncks -A frseaice.nc4 merra2_201407${day}_surface.nc4
-    ncks -A cldtot.nc4 merra2_201407${day}_surface.nc4
+echo "  Processing levels data..."
+ncks -v T,QV,QL,QI,O3,CLOUD,DELP MERRA2_400.inst3_3d_asm_Nv.${year}${month}${day}.nc4 -o merra2_${year}${month}${day}_levels.nc4
+echo "    Renaming levels variables to match ERA..."
+ncrename -h -O -v T,t -v QV,q -v O3,o3 -v QL,clwc -v QI,ciwc -v CLOUD,cc -v DELP,delp merra2_${year}${month}${day}_levels.nc4
+# rename dimension/variables (subject to bug in netcdf library where renaming either dimension or variable renames both. see http://nco.sourceforge.net/nco.html#ncrename-netCDF-Renamer)
+ncrename -O -h -v lon,longitude -v lat,latitude -v lev,level merra2_${year}${month}${day}_levels.nc4
 
-    echo "    Removing temporary files..."
-    rm frseaice.nc4 cldtot.nc4
+echo "  Processing surface data..."
+echo "    Extracting variables..."
+# to extract hours to match era using stride like "ncks -d time,0,,3 ..."
+ncks -v CLDTOT MERRA2_400.tavg1_2d_rad_Nx.${year}${month}${day}.nc4 -o cldtot.nc4
+ncks -v FRSEAICE MERRA2_400.tavg1_2d_flx_Nx.${year}${month}${day}.nc4 -o frseaice.nc4
+ncks -v PS,U10M,V10M,TS MERRA2_400.inst1_2d_asm_Nx.${year}${month}${day}.nc4 -o merra2_${year}${month}${day}_surface.nc4
 
-    echo "    Renaming surface variables to match ERA..."
-    ncrename -h -O -v FRSEAICE,ci -v PS,sp -v CLDTOT,tcc -v U10M,u10m -v V10M,v10m -vTS,skt merra2_201407${day}_surface.nc4
-    
-    echo "Day $day done"
-done
+echo "    Appending variables..."
+ncks -A frseaice.nc4 merra2_${year}${month}${day}_surface.nc4
+ncks -A cldtot.nc4 merra2_${year}${month}${day}_surface.nc4
+
+echo "    Removing temporary files..."
+rm frseaice.nc4 cldtot.nc4
+
+echo "    Renaming surface variables to match ERA..."
+ncrename -h -O -v FRSEAICE,ci -v PS,sp -v CLDTOT,tcc -v U10M,u10 -v V10M,v10 -vTS,skt merra2_${year}${month}${day}_surface.nc4
+# rename dimension/variables (subject to bug in netcdf library where renaming either dimension or variable renames both. see http://nco.sourceforge.net/nco.html#ncrename-netCDF-Renamer)
+ncrename -O -h -v lon,longitude -v lat,latitude merra2_${year}${month}${day}_surface.nc4
+
+echo "Day $day done"
+
